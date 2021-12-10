c6dd952b7a7f8044449908e1d11c8cca04aafa98
Vladislaw1 <48916709+Vladislaw1@users.noreply.github.com>
2021-12-10 10:44:15 +0200

    v.0.2

--- src/shared/service/base-http-service.js
+++ src/shared/service/base-http-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export class BaseHttpService {
-    BASE_URL = "http://api.openweathermap.org/data/2.5";
+    BASE_URL = "https://api.openweathermap.org/data/2.5";
     USER_KEY = "b5c6ee162439d799c35b2730084db6e5"
 
     async get(endpoint){
